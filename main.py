97f37169d380caa1ddd5685bfab69ba69ab2856d
yhyu13 <yohan680919@gmail.com>
2018-06-25 11:27:33 -0500

    Add multicore trainer

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from MCTSnet import MCTSnet
 from Connect4 import Connect4
 import config
 import numpy as np
-import pickle
 from IPython.core.debugger import set_trace
 import torch
 import utils
 from MCTSnet_trainer import Trainer
+import multiprocessing as mp
 
 import argparse
 
@@ -14,33 +14,53 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--func', default='self_play', help='Choose between self_play and play_minmax')
 args = parser.parse_args()
 
-connect4 = Connect4()
-actions = connect4.actions
-get_legal_actions = connect4.get_legal_actions
-transition_and_evaluate = connect4.transition_and_evaluate
+if __name__ == '__main__':
+    ctx = mp.get_context('spawn')
 
-root_state = np.zeros(shape=(3, 6, 7), dtype="float32")
-iteration = 0
+    connect4 = Connect4()
+    actions = connect4.actions
+    get_legal_actions = connect4.get_legal_actions
+    transition_and_evaluate = connect4.transition_and_evaluate
 
-mctsnet = MCTSnet(actions=actions, get_legal_actions=get_legal_actions,
-    transition_and_evaluate=transition_and_evaluate)
+    root_state = np.zeros(shape=(3, 6, 7), dtype="float32")
+    iteration = 0
 
-memories = utils.load_memories()
+    mctsnet = MCTSnet(actions=actions, get_legal_actions=get_legal_actions,
+        transition_and_evaluate=transition_and_evaluate)
 
-trainer = Trainer()
+    memories = utils.load_memories()
 
-funcs = {'self_play': mctsnet.self_play, 'play_minmax': mctsnet.play_minmax}
-F = funcs[args.func]
+    trainer = Trainer()
+    # define function references
+    funcs = {'self_play': mctsnet.self_play, 'play_minmax': mctsnet.play_minmax}
+    F = funcs[args.func]
+    G = mctsnet.tournament
 
-while True:
-    new_memories, _ = F(root_state)
-    memories.extend(new_memories)
+    num_worker = 4
+    pool = ctx.Pool(processes=num_worker)
 
-    utils.save_memories(memories)
+    # multicore trainer
+    def multi_trainer():
+        res = pool.map(F, [root_state]*(num_worker-1))
+        for tup in res:
+            # tup[0] === new_memories
+            memories.extend(tup[0])
+        utils.save_memories(memories)
+        trainer.fastai_train(mctsnet.new, memories)
+        pool.apply_async(G, (root_state,))
 
-    trainer.fastai_train(mctsnet.new, memories)
+    while iteration <= 5:
+        """
+        # single process trainer & evaluator
+        new_memories, _ = F(root_state)
+        memories.extend(new_memories)
+        utils.save_memories(memories)
+        trainer.fastai_train(mctsnet.new, memories)
+        G(root_state)
+        """
+        multi_trainer()
+        iteration += 1
+        print("Iteration Number "+str(iteration))
 
-    mctsnet.tournament(root_state)
-
-    iteration += 1
-    print("Iteration Number "+str(iteration))
+    pool.join()
+    pool.close()
